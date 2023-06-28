e553c4c88571be76b5da6c3826de35d21b6497f2
Mahideep <mahipadala206@gmail.com>
2023-06-29 00:13:22 +0530

    finished

--- src/Contact2.jsx
+++ src/Contact2.jsx
@@ -3,15 +3,31 @@
 const contact2 = () => {
   return (
     <div className="contact2">
-      <form className="form">
+      <form
+        className="form"
+        action="https://getform.io/f/717140a8-2ac1-4c5e-8988-65fe61d7a0a3"
+        method="post"
+      >
         <label>
-          <input type="text" required="" placeholder="" className="input" />
-          <span>first name</span>
+          <input
+            type="text"
+            required=""
+            placeholder=""
+            className="input"
+            name="name"
+          />
+          <span>Name</span>
         </label>
 
         <label>
-          <input required="" placeholder="" type="email" className="input" />
-          <span>email</span>
+          <input
+            required=""
+            placeholder=""
+            type="email"
+            className="input"
+            name="email"
+          />
+          <span>Email</span>
         </label>
 
         <label>
@@ -20,8 +36,9 @@ const contact2 = () => {
             rows="3"
             placeholder=""
             className="input01"
+            name="message"
           ></textarea>
-          <span>message</span>
+          <span>Message</span>
         </label>
 
         <button className="fancy" href="#">
--- src/assets/components/SocialBar.jsx
+++ src/assets/components/SocialBar.jsx
@@ -4,7 +4,7 @@ const SocialBar = () => {
   return (
     <div>
       <div className="socialbar">
-        <a id="github" href="#">
+        <a id="github" href="https://github.com/PadalaMahideep">
           <svg
             viewBox="0 0 16 16"
             className="bi bi-github"
@@ -17,7 +17,10 @@ const SocialBar = () => {
           </svg>
         </a>
         &nbsp; &nbsp; &nbsp;
-        <a id="instagram" href="#">
+        <a
+          id="instagram"
+          href="https://www.instagram.com/mahideep___/?igshid=OGQ5ZDc2ODk2ZA%3D%3D"
+        >
           <svg
             viewBox="0 0 16 16"
             className="bi bi-instagram"
@@ -30,29 +33,25 @@ const SocialBar = () => {
           </svg>
         </a>
         &nbsp; &nbsp; &nbsp;
-        <a id="facebook" href="#">
+        <a
+          id="facebook"
+          href="https://www.linkedin.com/in/mahideep-p-25220b1bb/"
+        >
           <svg
-            viewBox="0 0 16 16"
-            className="bi bi-facebook"
-            fill="currentColor"
-            height="25"
-            width="25"
             xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            className="feather feather-linkedin"
           >
-            <path d="M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951z"></path>
-          </svg>
-        </a>
-        &nbsp; &nbsp; &nbsp;
-        <a id="twitter" href="#">
-          <svg
-            viewBox="0 0 16 16"
-            className="bi bi-twitter"
-            fill="currentColor"
-            height="20"
-            width="20"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path d="M5.026 15c6.038 0 9.341-5.003 9.341-9.334 0-.14 0-.282-.006-.422A6.685 6.685 0 0 0 16 3.542a6.658 6.658 0 0 1-1.889.518 3.301 3.301 0 0 0 1.447-1.817 6.533 6.533 0 0 1-2.087.793A3.286 3.286 0 0 0 7.875 6.03a9.325 9.325 0 0 1-6.767-3.429 3.289 3.289 0 0 0 1.018 4.382A3.323 3.323 0 0 1 .64 6.575v.045a3.288 3.288 0 0 0 2.632 3.218 3.203 3.203 0 0 1-.865.115 3.23 3.23 0 0 1-.614-.057 3.283 3.283 0 0 0 3.067 2.277A6.588 6.588 0 0 1 .78 13.58a6.32 6.32 0 0 1-.78-.045A9.344 9.344 0 0 0 5.026 15z"></path>
+            <path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z"></path>
+            <rect x="2" y="9" width="4" height="12"></rect>
+            <circle cx="4" cy="4" r="2"></circle>
           </svg>
         </a>
       </div>
